082406196dfabc232782043ef314225320379bb8
Dylan Fox <dylan.fox@colorado.edu>
2020-12-30 15:31:33 -0700

    refactoring tests for moving var decls

--- tests/move_var_decls_to_top_test.py
+++ tests/move_var_decls_to_top_test.py
@@ -1,59 +1,24 @@
 import ast
 import astor
+import pytest
 
 from pytransformation import _move_var_decls_to_top_of_scope
+from .test_file_utilities import get_all_test_and_result_files
 
+path_to_test_dir = "./test_files/move_var_decls/"
 
-def test_move_var_decls_simple():
-    node = ast.parse('a = 5\nb=2\na =b')
+@pytest.fixture(params=get_all_test_and_result_files(path_to_test_dir))
+def test_and_result_strings(request):
+    return request.param
 
-    #Input Code:
-    #   a = 5
-    #   b = 2
-    #   a = b
+def test_move_var_decls_to_top(test_and_result_strings):
+    test_string = test_and_result_strings[0]
+    result_string = test_and_result_strings[1]
 
-    result_string, names_used = _move_var_decls_to_top_of_scope(node, {})
-    astor_source_string_result = astor.to_source(result_string)
-
-    test_string = 'a = None\nb = None\na = 5\nb = 2\na = b\n'
-    #Expected Output Code:
-    #   a = None
-    #   b = None
-    #   a = 5
-    #   b = 2
-    #   a = b
-    assert(astor_source_string_result == test_string)
-
-
-
-# def test_move_var_decls_multiple_scope_levels():
-#     node = ast.parse('c=5 \ndef test():\n\tc = 5 \n\ta=5 \n\tif a == 5: \n\t\tb = 5 \nd=12 \nd += 12')
-#     #Source:
-#     #   c = 5
-#     #   def test():
-#     #       c = 5
-#     #       a = 5
-#     #       if a == 5:
-#     #           b = 5
-#     #   d=12
-#     #   d += 12
-#     result_string, names_used  = _move_var_decls_to_top_of_scope(node, {})
-#     astor_source_string_result = astor.to_source(result_string)
-#     test_string = 'c = None\nd = None\nc = 5\n\n\n' + \
-#         'def test():\n    a = None\n    b = None\n    c = 5\n    a = 5\n' + \
-#         '    if a == 5:\n        b = 5\n\n\nd = 12\nd += 12\n'
-#
-#     #Expected Output Source Code:
-#     #   c = None
-#     #   d = None
-#     #   c = 5
-#     #   def test():
-#     #       a = None
-#     #       b = None
-#     #       c = 5
-#     #       a = 5
-#     #       if a == 5:
-#     #           b = 5
-#     #   d = 12
-#     #   d += 12
-#     assert(astor_source_string_result == test_string)
+    test_ast = ast.parse(test_string)
+    if isinstance(test_ast.body[0], ast.FunctionDef):
+        test_ast = test_ast.body[0]
+    test_result_ast, _ = _move_var_decls_to_top_of_scope(test_ast, {})
+    test_result_string = astor.to_source(test_result_ast)
+    print(test_result_string)
+    assert(test_result_string == result_string)
