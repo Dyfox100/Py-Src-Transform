34ba34e1ece6d6bfeb07ea2e365349981cb38a3c
Dylan Fox <dylan.fox@colorado.edu>
2021-01-01 14:14:33 -0700

    changing some formatting

--- tests/test_file_utilities.py
+++ tests/test_file_utilities.py
@@ -15,10 +15,12 @@ def get_all_test_and_result_files(dir_name):
                   if os.path.isfile(os.path.join(absolute_path, f))]
     test_files = filter(lambda f: "test" in f, file_names)
 
+
     def get_number_from_test_file(file_name):
         file_without_dot_py = file_name.split(".py")[0]
         file_without_test_prefix = file_without_dot_py.split("_")[1]
         return int(file_without_test_prefix)
+
     try:
         list_of_test_numbers = map(get_number_from_test_file, test_files)
     except ValueError as e:
@@ -40,7 +42,6 @@ def get_all_test_and_result_files(dir_name):
 
             test_strings.append(test.read())
             result_strings.append(result.read())
-
     except IOError as e:
         print("File not found!")
         print("Do all of your test files have properly named result files?")
