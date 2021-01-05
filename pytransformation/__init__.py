6a96a5a654dfb304bbcd6933214cf7ca716f3896
Dylan Fox <dylan.fox@colorado.edu>
2021-01-05 08:59:59 -0700

    adding top level entry point that calls transforms at each scope.

--- pytransformation/__init__.py
+++ pytransformation/__init__.py
@@ -4,7 +4,7 @@ from .move_var_decls_to_top_of_scope import _move_var_decls_to_top_of_scope
 from .name_function_applications import _name_unnamed_applications
 from .name_utils import _new_name
 from .name_utils import _get_all_used_variable_names
-
+from .transform import Source_Code_Transformer
 
 __all__ = [
     _get_all_used_variable_names,
@@ -12,5 +12,6 @@ __all__ = [
     _move_var_decls_to_top_of_scope,
     _name_unnamed_applications,
     _new_name,
-    normalize_to_ANF
+    normalize_to_ANF,
+    Source_Code_Transformer
 ]
